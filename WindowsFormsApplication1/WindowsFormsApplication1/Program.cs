a71d41fdaedfad77762c723f9b51a551aab80cac
ono <ono.miyoko@fujitsu-general.com>
2020-10-02 13:19:34 +0900

    変更

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            int i = 0;
+
+            System.Console.WriteLine("ループ開始");
+            while (i < 20)
+            {
+                System.Console.WriteLine("iの値は" + i);
+                i++;
+            }
+            System.Console.WriteLine("ループ終了");
+        }
+    }
+}
